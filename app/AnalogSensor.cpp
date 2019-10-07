5f79358ad9b67610413d54be9a4bc7b321283d3d
Rohan Singh <rohansingh.apjss@gmail.com>
2019-10-07 01:31:55 -0400

    After Bug fix

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,6 +1,7 @@
 #include <AnalogSensor.hpp>
 #include <numeric>
 #include <vector>
+#include <memory>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
     : mSamples(samples)
@@ -13,7 +14,7 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+    std::shared_ptr<std::vector<unsigned int>> readings (new std::vector<unsigned int>(mSamples, 10));      // Bug fix - Used smart pointers
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
